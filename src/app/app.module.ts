4f921d5c4f5e8a01c6297409004dd519d0ff582c
Serhii Nykyforov <04mailbox@gmail.com>
2018-05-26 16:59:28 +0300

    Add UserDictionary component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,12 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { UserDictionaryComponent } from './user-dictionary/user-dictionary.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    UserDictionaryComponent
   ],
   imports: [
     BrowserModule
---
+++ src/app/user-dictionary/user-dictionary.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'user-dictionary',
+  templateUrl: './user-dictionary.component.html',
+  styleUrls: ['./user-dictionary.component.css']
+})
+export class UserDictionaryComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
