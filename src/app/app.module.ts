e6aef5d1935d2aab80add737bd9012af596ecefb
Serhii Nykyforov <04mailbox@gmail.com>
2018-06-02 18:52:07 +0300

    Add Setting component with language select option

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,15 +6,19 @@ import { MatListModule } from '@angular/material/list';
 import { MatDividerModule } from '@angular/material/divider';
 import { MatButtonModule } from '@angular/material/button';
 import { MatIconModule } from '@angular/material/icon';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatSelectModule } from '@angular/material/select';
 
 import { AppComponent } from './app.component';
 import { UserDictionaryComponent } from './user-dictionary/user-dictionary.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { SettingsComponent } from './settings/settings.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    UserDictionaryComponent
+    UserDictionaryComponent,
+    SettingsComponent
   ],
   imports: [
     BrowserModule,
@@ -25,6 +29,8 @@ import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
     MatDividerModule,
     MatButtonModule,
     MatIconModule,
+    MatFormFieldModule,
+    MatSelectModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
