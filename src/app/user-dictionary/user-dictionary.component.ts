5399add90a0aa0db62f91562e759ba0475526c72
Serhii Nykyforov <04mailbox@gmail.com>
2018-06-10 18:56:50 +0300

    Not translate word  on init of UserDictionaryComponent

--- src/app/user-dictionary/user-dictionary.component.ts
+++ src/app/user-dictionary/user-dictionary.component.ts
@@ -1,4 +1,4 @@
-import {Component, OnInit} from '@angular/core';
+import {Component} from '@angular/core';
 import {DictionaryEntry} from '../dictionary-entry';
 import {DICTIONARY} from '../mock-dictionary';
 import {TranslationService} from "../translation.service";
@@ -8,7 +8,7 @@ import {TranslationService} from "../translation.service";
   templateUrl: './user-dictionary.component.html',
   styleUrls: ['./user-dictionary.component.css']
 })
-export class UserDictionaryComponent implements OnInit {
+export class UserDictionaryComponent {
   dictionary = DICTIONARY;
 
   selectedEntry: DictionaryEntry;
@@ -16,22 +16,7 @@ export class UserDictionaryComponent implements OnInit {
   constructor(private translationService: TranslationService) {
   }
 
-  ngOnInit() {
-    this.translateHello();
-  }
-
   selectEntry(entry: DictionaryEntry) {
     this.selectedEntry = entry;
-  }
-
-  private translateHello() {
-    let word = 'hello';
-    this.translationService.translate(word)
-      .subscribe(translation => {
-        console.log(translation);
-        if (translation.length > 0) {
-          this.dictionary.push({id: 42, word, translation: translation[0]});
-        }
-      });
   }
 }
